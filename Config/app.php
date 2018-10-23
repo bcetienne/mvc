9ab454c64d1260be7fc1a5e550cf8684d603b3e3
Etienne BC <etienne1296@gmail.com>
2018-10-23 12:42:05 +0000

    Remove

--- Config/app.php
+++ Config/app.php
@@ -13,14 +13,3 @@
       'password' => 'root'
     ]
   ];
-
-
-  CREATE TABLE salaries
-  (
-      id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-      firstname VARCHAR(100),    ->     lastname VARCHAR(100),
-      email VARCHAR(255),
-      salaire SMALLINT
-  );
-
-  INSERT INTO salaries (firstname, lastname, email, salaire) VALUES ('Maxime', 'Larousse', 'maxime@mail.com', 1546), ('Jean', 'Bechrel', 'jean@mail.com', 1852), ('Lea', 'Atlas', 'atlas@mail.com', 2000)
