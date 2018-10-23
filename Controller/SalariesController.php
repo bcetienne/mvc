f4f3cbb937fc9d599bb392e22a25ea69facdcb7b
Etienne BC <etienne1296@gmail.com>
2018-10-23 14:28:56 +0000

    Add basics fctn

--- Controller/SalariesController.php
+++ Controller/SalariesController.php
@@ -4,5 +4,21 @@
    */
   class SalariesController 
   {
+
+    public function index() {
+      var_dump('index salaries');
+    }
+
+    public function add() {
+      var_dump('add salaries');
+    }
+
+    public function edit() {
+      var_dump('edit salaries');
+    }
+
+    public function remove() {
+      var_dump('remove salaries');
+    }
     
   }
