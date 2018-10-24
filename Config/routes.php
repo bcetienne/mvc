e2116ef1f0ce827d0a4fc02d82285ac63b2d7e50
Etienne BC <etienne1296@gmail.com>
2018-10-24 14:59:26 +0000

    Add default route for '/'

--- Config/routes.php
+++ Config/routes.php
@@ -3,6 +3,10 @@
 
   // Retrieve route from URL
   $router = new Router();
+
+  if ($_SERVER['REQUEST_URI'] == '/') {
+    header('Location: /salaries');
+  }
   
   // Make all routes needed
   $router->redirect('/salaries', ['controller' => 'Salaries', 'action' => 'index']);
